ece55a2a44597f2ff0f84a60b5a0c4f20d12becf
az0819 <az.keuo@gmail.com>
2025-11-29 13:18:31 -0500

    Update llm_server.py

--- llm_server.py
+++ llm_server.py
@@ -26,27 +26,25 @@ if __name__ == '__main__':
     # client_sock = socket.accept
 
     while True:
-        """
-        will only have one client
-        but has to serve responses for multiple profiles
-        in proxy we can inject http header to have profile info and client id, 
-        maybe we can also inject with a feedback line if client is like too many words bolded or smthn like that
+        # will only have one client
+        # but has to serve responses for multiple profiles
+        # in proxy we can inject http header to have profile info and client id, 
+        # maybe we can also inject with a feedback line if client is like too many words bolded or smthn like that
 
-        socket.read
-        figure out python way to parse http message this must be way easier surely
-        note that everything you read will be 200 OK webpages
+        # socket.read
+        # figure out python way to parse http message this must be way easier surely
+        # note that everything you read will be 200 OK webpages
 
-        if complete message, parse out profile info
-        feed body to llm using profile parameters
-        if feedback header, modify prompt/make it so all future stuff for this profile has this change
-            /if we are doing predetermined options for feedback have a majority win system
-            where the majority determines the prompt modification
-        get back augmented webpage
+        # if complete message, parse out profile info
+        # feed body to llm using profile parameters
+        # if feedback header, modify prompt/make it so all future stuff for this profile has this change
+        #     /if we are doing predetermined options for feedback have a majority win system
+        #     where the majority determines the prompt modification
+        # get back augmented webpage
 
-        socket.write to client
+        # socket.write to client
 
-        i have no idea how we are going to get interactive info from client in the proxy though
+        # i have no idea how we are going to get interactive info from client in the proxy though
 
         
-        in the proxy, when you accept new clients, immediately send back some kind of loading page
-        """
+        # in the proxy, when you accept new clients, immediately send back some kind of loading page
