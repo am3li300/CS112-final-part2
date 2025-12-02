43837213f69bd961538aea54a706db0220c117f1
az0819 <az.keuo@gmail.com>
2025-12-02 16:07:16 -0500

    start

--- llm_server.py
+++ llm_server.py
@@ -2,32 +2,92 @@ from llmproxy import LLMProxy
 import os
 import sys
 import socket
+import requests
+from bs4 import BeautifulSoup
 
 """
-Usage: python llm_server.py <listen_port> <proxy_ip> <proxy_port>
+Usage: python llm_server.py <listen_port>
 """
 if __name__ == '__main__':
-    print("Hello World")
-    assert sys.argc == 4
+    if len(sys.argv) != 2:
+        print("Usage: python llm_server.py <listen_port>")
+        sys.exit(1)
     # make this nicer? print an error message with usage?
 
-    listen_port = sys.argv[1]
-    proxy_ip = sys.argv[2]
-    proxy_port = sys.argv[3]
-    # do some kind of type checking?
+    # listen_port = int(sys.argv[1])
 
-    main_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    main_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    main_sock.bind(socket.INADDR_ANY, listen_port)
-    main_sock.listen(5)
+    # main_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # main_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # main_sock.bind(('', listen_port))
+    # main_sock.listen(5)
 
-    llm = LLMProxy()
+    # llm = LLMProxy()
 
-    # client_sock = socket.accept
+    # client_sock, client_addr = main_sock.accept()
 
-    while True:
+
+
+    resp = requests.get("https://example.com")
+    html = resp.text
+    headers = resp.headers
+    
+    soup = BeautifulSoup(html, "html.parser")
+
+    title_tag = soup.title
+    print(f"Title: {title_tag.string}")
+
+    # while True:
+        """
+        1) do a read, get some response message
+        
+        2) parse the response headers - make sure its actually the html file
+
+        3) get the html body 
+
+        4) feed html into llm
+
+        5) return to proxy
+
+        PROXY SIDE - need to implement usage of LLM server
+            1) every time a server writes to client, send it through the LLM before 
+            forwarding
+
+
+        """
+        # buf = client_sock.recv(8192)
+
+        # if len(buf) > 0:
+        #     print(buf.decode('utf-8'))
+
+        # header, body = raw.split(b"\r\n\r\n", 1)
+
+        # html = body.decode("utf-8", errors="ignore")
+        # headers = header_bytes.decode(errors="ignore")
+
+
+
+
+        # """
+        # parse http message into some structure
+        # feed body to llm
+        # replace body in structure
+        # """
+
+        # body = str()
+        
+        # response = client.generate(
+        #     model="us.anthropic.claude-3-haiku-20240307-v1:0",
+        #     system="Break up this html into more readable chunks, bolding the most important words. Do not change the actual words.",
+        #     query=body,
+        #     lastk=0
+        # )
+
+        
+
+
+        # """
         # will only have one client
-        # but has to serve responses for multiple profiles
+        # //but has to serve responses for multiple profiles
         # in proxy we can inject http header to have profile info and client id, 
         # maybe we can also inject with a feedback line if client is like too many words bolded or smthn like that
 
@@ -48,3 +108,4 @@ if __name__ == '__main__':
 
         
         # in the proxy, when you accept new clients, immediately send back some kind of loading page
+        # """
